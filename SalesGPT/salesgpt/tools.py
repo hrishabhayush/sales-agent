24d7add80ce62ab283b340bcbc15e53dfc6f00af
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-06-12 15:12:55 -0400

    changed the post_twitter_post function mimicing the exact functionality of Twitter docs Manage-Tweets/create_tweet.py

--- SalesGPT/salesgpt/tools.py
+++ SalesGPT/salesgpt/tools.py
@@ -282,48 +282,71 @@ def get_twitter_content_from_query(query):
 
 def post_twitter_post(content_with_hashtags):
     '''Post a twitter post based on the single query string using OAuth1Session'''
+    consumer_key = os.getenv("TWITTER_API_KEY")
+    consumer_secret = os.getenv("TWITTER_API_KEY_SECRET")
+
+    # Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
+    payload = {"text": "Hello world!"}
+
+    # Get request token
+    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
     try:
-        # Get Twitter API credentials from environment variables
-        consumer_key = os.getenv("TWITTER_API_KEY")
-        consumer_secret = os.getenv("TWITTER_API_SECRET_KEY")
-        access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-        access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-        
-        # Check if we have the required credentials
-        if not all([consumer_key, consumer_secret, access_token, access_token_secret]):
-            return "Missing Twitter API credentials - running in TEST MODE\n. Would post: " + content_with_hashtags
-        
-        # Prepare the tweet payload
-        payload = {"text": content_with_hashtags}
-        
-        # Create OAuth1Session with access tokens
-        oauth = OAuth1Session(
-            consumer_key,
-            client_secret=consumer_secret,
-            resource_owner_key=access_token,
-            resource_owner_secret=access_token_secret,
+        fetch_response = oauth.fetch_request_token(request_token_url)
+    except ValueError:
+        print(
+            "There may have been an issue with the consumer_key or consumer_secret you entered."
         )
-        
-        # Make the request to Twitter API v2
-        response = oauth.post(
-            "https://api.twitter.com/2/tweets",
-            json=payload,
+
+    resource_owner_key = fetch_response.get("oauth_token")
+    resource_owner_secret = fetch_response.get("oauth_token_secret")
+    print("Got OAuth token: %s" % resource_owner_key)
+
+    # Get authorization
+    base_authorization_url = "https://api.twitter.com/oauth/authorize"
+    authorization_url = oauth.authorization_url(base_authorization_url)
+    print("Please go here and authorize: %s" % authorization_url)
+    verifier = input("Paste the PIN here: ")
+
+    # Get the access token
+    access_token_url = "https://api.twitter.com/oauth/access_token"
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=verifier,
+    )
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+    access_token = oauth_tokens["oauth_token"]
+    access_token_secret = oauth_tokens["oauth_token_secret"]
+
+    # Make the request
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
+    )
+
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
+
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
         )
-        
-        # Check response status
-        if response.status_code == 201:
-            json_response = response.json()
-            tweet_id = json_response.get('data', {}).get('id', 'unknown')
-            return f"Twitter post posted successfully! Tweet ID: {tweet_id}"
-        else:
-            # Handle different error cases
-            if response.status_code == 403:
-                return f"TWITTER API ACCESS ISSUE - Running in TEST MODE\n📝 Generated tweet: {content_with_hashtags}\n💡 To post for real, upgrade your Twitter API access at: https://developer.x.com/en/portal/product\n🔍 Error details: {response.text}"
-            else:
-                return f"Twitter API error (status {response.status_code}): {response.text}"
-    
-    except Exception as e:
-        return f"Twitter post was not posted successfully, error: {e}"
+
+    print("Response code: {}".format(response.status_code))
+
+    # Saving the response as JSON
+    json_response = response.json()
+    print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
 def generate_twitter_post(query):
