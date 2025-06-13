530c8b17417aa893b936407d68999c626d375ff9
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-06-13 11:27:35 -0400

    updated frontend components to demo sales agent functionality like a chatbot

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,19 +1,26 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import React, { useState } from 'react'
 
 export default function Main() {
-    const [hasEntered, setEntered] = useState(false);
+
+    const [hasSent, setSent] = useState(false)
 
     return (
         <div className="flex items-center justify-center min-h-screen">
             <div className="grid w-full max-w-md gap-1">
                 <Label htmlFor="message">Enter your prompt here to be posted on Twitter</Label>
                 <Textarea placeholder="Type your message here." />
-                <Button>Send message</Button>
+                <Button 
+                    onClick={() => setSent(true)}
+                    disabled = {hasSent}
+                >
+                    {hasSent ? "Message Sent" : "Send message"}
+                </Button>
             </div>
         </div>
     )
-
 }
